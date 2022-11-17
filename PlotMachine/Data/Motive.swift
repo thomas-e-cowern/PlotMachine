62fea73ad7e218dadc6cb09e530898dee4dad5ea
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-17 02:01:59 -0500

    add motive to Data folder

---
+++ PlotMachine/Data/Motive.swift
@@ -0,0 +1,16 @@
+//
+//  Motive.swift
+//  PlotMachine
+//
+//  Created by Thomas Cowern on 11/17/22.
+//
+
+import Foundation
+
+var motive: [Int : String] = [
+    1 : "Money",
+    2 : "Power",
+    3 : "Revenge",
+    4 : "Sex",
+    5 : "Fame"
+]
