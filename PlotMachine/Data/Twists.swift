6365b95c437eb13a71158c46b546e6e74f49706e
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-17 11:01:41 -0500

    add Twists to Data folder

---
+++ PlotMachine/Data/Twists.swift
@@ -0,0 +1,35 @@
+//
+//  Twists.swift
+//  PlotMachine
+//
+//  Created by Thomas Cowern on 11/17/22.
+//
+
+import Foundation
+
+var twists: [Int : String] = [
+    1 : "Somebody is dead you didn’t expect",
+    2 : "Getting Knocked Out",
+    3 : "Planted a bomb",
+    4 : "Anonymous text of email",
+    5 : "A dark secret revealed",
+    6 : "An emotional wound revealed",
+    7 : "An ally betrays",
+    8 : "A guy with a gun walks in",
+    9 : "The road is closed",
+    10 : "The bridge is out",
+    11 : "Thought it was a man, but it was a woman",
+    12 : "Thought it was a woman, but it was a man",
+    13 : "Thought a character was dead, but wasn’t",
+    14 : "Surprise Witness",
+    15 : "Message didn’t go through",
+    16 : "Corrupt person is trustworthy",
+    17 : "Trustworthy person is corrupt",
+    18 : "Protagonist finds out related to another character",
+    19 : "Accident",
+    20 : "Injury",
+    21 : "Something false goes viral",
+    22 : "Good news goes bad",
+    23 : "Bad news gets worse"
+]
+
