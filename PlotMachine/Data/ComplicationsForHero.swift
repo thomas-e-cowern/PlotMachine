2eadbca1fe90f3981def4eb570433126b9ff246b
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-17 02:29:56 -0500

    add ComplicationsForHero to Data folder

---
+++ PlotMachine/Data/ComplicationsForHero.swift
@@ -0,0 +1,18 @@
+//
+//  ComplicationsForHero.swift
+//  PlotMachine
+//
+//  Created by Thomas Cowern on 11/17/22.
+//
+
+import Foundation
+
+var ComplicationsForHero: [Int : String] = [
+    1: "Relative",
+    2: "Friend",
+    3: "Alcoholic",
+    4: "Cowboy",
+    5: "Architect",
+    6: "Fighter",
+    7: "Health nut"
+]
