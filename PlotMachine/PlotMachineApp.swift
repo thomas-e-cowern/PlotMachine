88cd419b67a8dc67269fbfb38c8fe6d17e977986
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-17 01:48:03 -0500

    first commit

---
+++ PlotMachine/PlotMachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlotMachineApp.swift
+//  PlotMachine
+//
+//  Created by Thomas Cowern on 11/17/22.
+//
+
+import SwiftUI
+
+@main
+struct PlotMachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
